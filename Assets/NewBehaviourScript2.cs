6d98731510218f30181ac5dd667d7c768804e6e9
brianyxu <brian.y.xu@vanderbilt.edu>
2017-11-16 10:40:40 -0600

    Text and spawn in one object

---
+++ Assets/NewBehaviourScript2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript2 : MonoBehaviour {
+	bool isCreated;
+	bool ready;
+	public GameObject sphere;
+	public GameObject walls;
+	public GameObject cube;
+	int count;
+	GameObject sphereClone;
+	// Use this for initialization
+	void Start () {
+		bool isCreated=false;
+		ready = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerPosition = Camera.main.transform.position;
+		Vector3 playerDirection = Camera.main.transform.forward;
+		Vector3 position = playerPosition + playerDirection*2;
+		if (!ready) {
+		}else {
+			sphereClone.transform.position = position;
+		}
+		if (Input.GetMouseButton (0)) {
+			if (!isCreated) {
+				sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+				sphereClone = Instantiate (sphere, position, Quaternion.identity) as GameObject;
+				isCreated = true;
+				ready = true;
+			}
+		}
+	}
+}
